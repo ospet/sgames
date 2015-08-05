b7ef76e3d67313701bca35f5b63feac7ded262bb
Olivier Spet <olivier.spet@gmail.com>
2015-08-05 16:02:50 -0300

    Flex tables to display lists

--- frontend/js/app/controllers/activities/activitiesController.js
+++ frontend/js/app/controllers/activities/activitiesController.js
@@ -3,12 +3,8 @@ angular.module('Rubikar').controller('ActivitiesController', ['$scope', '$http',
   $scope.rawjson = '';
   $http.get('/apigdocs/0By1692QGa-25SFgtbVNsYVo4Mmc').
   then(function(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-    $scope.rawjson = response.data;
+    $scope.activities = angular.fromJson(response.data);
   }, function(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
     console.log('ActivitiesController - godcs call failed');
   });
   
